df01ef661e2d171fe2a92b4f0ce4b89f8a642af3
nourister <nourusterjuma@gmail.com>
2023-05-31 03:25:38 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,8 +1,5 @@
-#include "main.h"
-#include <stdarg.h>
 #include <stdio.h>
-
-int handle_format_specifier(char specifier, va_list my_args, int *counter);
+#include <stdarg.h>
 
 /**
  * _printf - print function
@@ -10,118 +7,177 @@ int handle_format_specifier(char specifier, va_list my_args, int *counter);
  * @...:indicates any number of arguments to be passed
  * Return: printed character
  */
-
 int _printf(const char *format, ...)
 {
-	int counter = 0;
-	int n = 0;
-
 	va_list my_args;
+	int counter = 0;
 
 	va_start(my_args, format);
 
-	while (format[n])
+	while (*format)
 	{
-		if (format[n] != '%')
+		if (*format == '%')
 		{
-			_putchar(format[n]);
-			counter++;
-		}
-		else
-		{
-			n++;
-			counter += handle_format_specifier(format[n], my_args, &counter);
-		}
-		n++;
-	}
+			format++;
+			if (*format == '\0')
+                break;
 
-	va_end(my_args);
-	return (counter);
-}
-
-/**
- * handle_format_specifier - Handles the conversion specifier.
- * @specifier: The conversion specifier character.
- * @my_args: The va_list of arguments
- * @counter: The current counter value.
- * Return: The updated counter value.
- */
-int handle_format_specifier(char specifier, va_list my_args, int *counter)
-{
-	switch (specifier)
-	{
-		case 'c':
-			return (_putchar(va_arg(my_args, int)));
-		case 's':
-			return (print_string(va_arg(my_args, char*)));
-		case 'd':
-		case 'i':
-			if (length_modifier == 'l')
-				return (print_long_decimal(va_arg(my_args, long)));
-			else if (length_modifier == 'h')
-				return (print_short_decimal(va_arg(my_args, int)));
-			else
-				return (print_decimal(va_arg(my_args, int)));
-		case 'u':
-			if (length_modifier == 'l')
-				return (print_long_unsigned(va_arg(my_args, unsigned long)));
-			else if (length_modifier == 'h')
-				return (print_short_unsigned(va_arg(my_args, unsigned int)));
-			else
-				return (print_unsigned(va_arg(my_args, unsigned int)));
-		case 'o':
-			if (length_modifier == 'l')
-				return (print_long_octal(va_arg(my_args, unsigned long)));
-			else if (length_modifier == 'h')
-				return (print_short_octal(va_arg(my_args, unsigned int)));
-			else
-				return (print_octal(va_arg(my_args, unsigned int)));
-		case 'x':
-		case 'X':
-			if (length_modifier == 'l')
-				return (print_long_hexadecimal(va_arg(my_args, unsigned long), specifier));
-			else if (length_modifier == 'h')
-				return (print_short_hexadecimal(va_arg(my_args, unsigned int), specifier));
-			else
-				return (print_hexadecimal(va_arg(my_args, unsigned int), specifier));
-		case 'p':
-			return (print_address(va_arg(my_args, void *)));
-		case '%':
-			_putchar('%');
-			return (1);
-		case 'n':
-			{
-				int *ptr = va_arg(my_args, int *);
-				*ptr = *counter;
-			}
-			return (0);
-		case 'S':
-			return (print_custom_string(va_arg(my_args, char*)));
-		case '\0':
-			return (-1);
-		default:
-			_putchar('%');
-			_putchar(specifier);
-			return (2);
-	}
-}
-
-/**
- * flush_buffer - Writes the buffer contents to the output and resets the buffer.
- * @buffer: The buffer containing the characters to be written.
- * @buf_index: The current index in the buffer.
- * @counter: The current counter value.
- */
-void flush_buffer(char *buffer, int *buf_index, int *counter)
-{
-    int i;
-
-    for (i = 0; i < *buf_index; i++)
-    {
-        _putchar(buffer[i]);
+            switch (*format)
+            {
+                case 'c':
+                {
+                    char c = (char)va_arg(my_args, int);
+                    putchar(c);
+                    counter++;
+                    break;
+                }
+                case 's':
+                {
+                    char *str = va_arg(my_args, char *);
+                    while (*str)
+                    {
+                        putchar(*str);
+                        str++;
+                        counter++;
+                    }
+                    break;
+                }
+                case '%':
+                {
+                    putchar('%');
+                    counter++;
+                    break;
+                }
+                case 'd':
+                case 'i':
+                {
+                    int num = va_arg(my_args, int);
+                    printf("%d", num);
+                    counter += snprintf(NULL, 0, "%d", num);
+                    break;
+                }
+                case 'u':
+                {
+                    unsigned int num = va_arg(my_args, unsigned int);
+                    printf("%u", num);
+                    counter += snprintf(NULL, 0, "%u", num);
+                    break;
+                }
+                case 'o':
+                {
+                    unsigned int num = va_arg(my_args, unsigned int);
+                    printf("%o", num);
+                    counter += snprintf(NULL, 0, "%o", num);
+                    break;
+                }
+                case 'x':
+                {
+                    unsigned int num = va_arg(my_args, unsigned int);
+                    printf("%x", num);
+                    counter += snprintf(NULL, 0, "%x", num);
+                    break;
+                }
+                case 'X':
+                {
+                    unsigned int num = va_arg(my_args, unsigned int);
+                    printf("%X", num);
+                    counter += snprintf(NULL, 0, "%X", num);
+                    break;
+                }
+                case 'b':
+                {
+                    unsigned int num = va_arg(my_args, unsigned int);
+                    printf("%b", num);
+                    counter += snprintf(NULL, 0, "%b", num);
+                    break;
+                }
+                case 'S':
+                {
+                    char *str = va_arg(my_args, char *);
+                    while (*str)
+                    {
+                        if (*str < 32 || *str >= 127)
+                        {
+                            putchar('\\');
+                            putchar('x');
+                            printf("%02X", *str);
+                            counter += 4;
+                        }
+                        else
+                        {
+                            putchar(*str);
+                            counter++;
+                        }
+                        str++;
+                    }
+                    break;
+                }
+                case 'p':
+                {
+                    void *ptr = va_arg(my_args, void *);
+                    printf("%p", ptr);
+                    counter += snprintf(NULL, 0, "%p", ptr);
+                    break;
+                }
+                case 'r':
+                {
+                    char *str = va_arg(my_args, char *);
+                    int len = 0;
+		    int i = 0;
+                    while (str[len])
+                        len++;
+                    for (i = len - 1; i >= 0; i--)
+                    {
+                        putchar(str[i]);
+                        counter++;
+                    }
+                    break;
+                }
+                case 'R':
+                {
+                    char *str = va_arg(my_args, char *);
+                    while (*str)
+                    {
+                        char c = *str;
+                        if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+                            c += 13;
+                        else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+                            c -= 13;
+                        putchar(c);
+                        counter++;
+                        str++;
+                    }
+                    break;
+                }
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    counter += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+	    counter++;
+        }
+        format++;
     }
-
-    *counter += *buf_index;
-    *buf_index = 0;
-    memset(buffer, 0, sizeof(buffer));
+    
+    va_end(my_args);
+    
+    return (counter);
 }
+
+/*int main()
+{
+	_printf("Hello, %s! Today is %d-%d-%d\n", "John", 27, 5, 2023);
+	_printf("Binary: %b\n", 42);
+	_printf("String: %S\n", "Hello\nWorld");
+	_printf("Pointer: %p\n", &main);
+    _printf("Reversed: %r\n", "Hello");
+    _printf("ROT13: %R\n", "Hello");
+    
+    return 0;
+}*/
+
--- main.h
+++ main.h
@@ -1,53 +1,29 @@
-/* MAIN_H FILES */
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include "stdarg.h"
 
-#define BUFFER_SIZE 1024
+/**
+ * struct my_function_s - structure holding an identifier and a function
+ * @identifier: character identifying action to be used
+ * @print_my_function: function to excecuted when identifier is identified
+*/
 
-typedef struct specifier
+/*typedef struct my_function_s
 {
-	char specifier;
-	int (*function)(va_list);
-} specifier_t;
+	char *identifier;
+	int (*print_function)(va_list my_args);
+} my_function_t;*/
 
-
-int _putchar(char c);
-
+int main();
+/*int _putchar(char c);*/
 int _printf(const char *format, ...);
-
-int print_char(va_list my_args);
-
-int print_string(char *str);
-
-int print_int(va_list my_args);
-
-int print_number(int n);
-
-int handle_format_specifier(char specifier, va_list my_args, int *counter);
-
-unsigned int print_unsigned(va_list my_args);
-
-unsigned int print_unsigned_va_list(va_list my_args);
-
-unsigned int print_octal(va_list my_args);
-
-int print_address(void *p);
-
-int main(void);
-
-int handle_format_specifier(char specifier, va_list my_args, int *counter);
-
-void flush_buffer(char *buffer, int *buf_index, int *counter);
-
-unsigned int print_custom_string(char *str);
-
-unsigned int _putchar_hex(unsigned char c);
-
-unsigned int print_hexadecimal(va_list my_args, char specifier);
-unsigned int print_decimal(int num);
-
-#endif /* MAIN_H */
+/*int print_char(va_list ap);*/
+/*int printi(va_list ap);*/
+/*int print_int(int i);*/
+/*int print_string(va_list ap);*/
+/*int int_to_binary(int num);*/
+/*int int_to_bin(va_list ap);*/
+#endif /*_printf*/
