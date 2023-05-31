a3767168de5fa849c003278112cf11fc8a6c01e7
nourister <nourusterjuma@gmail.com>
2023-05-31 07:02:44 -0700

    update

--- my_functions.c
+++ my_functions.c
@@ -1,115 +1,120 @@
 #include "main.h"
+/**
+ * print_char - prints characters
+ * @ap: list of arguments
+ * Return: 1 (success)
+ */
 
-
-/* Sample print functions for different conversions */
-
-void printUnsigned(va_list my_args)
+int print_char(va_list ap)
 {
-    unsigned int num = va_arg(my_args, unsigned int);
-    printf("%u", num);
+	char c;
+
+	c = va_arg(ap, int);
+
+	return (_putchar(c));
 }
 
-void printOctal(va_list my_args)
+/**
+ * printi - prints integers
+ * @ap: list of arguments
+ * Return: 1 (success)
+ */
+
+int printi(va_list ap)
 {
-    unsigned int num = va_arg(my_args, unsigned int);
-    printf("%o", num);
+	int i = va_arg(ap, int);
+
+	return (print_int(i));
 }
 
-void printHex(va_list my_args)
+/**
+ * print_int - prints integers
+ * @i: integer
+ * Return: 1
+ */
+
+int print_int(int i)
 {
-    unsigned int num = va_arg(my_args, unsigned int);
-    printf("%x", num);
+	unsigned int num;
+	int count = 0;
+
+	if (i < 0)
+	{
+		count += _putchar('-');
+		num = i * -1;
+	}
+	else
+	{
+		num = i;
+	}
+
+	if (num >= 10)
+		count += print_int(num / 10);
+
+	count += _putchar((num % 10) + '0');
+
+	return (count);
 }
 
-void printHexUpper(va_list my_args)
+/**
+ * print_string - prints strings
+ * @ap: list of arguments
+ * Return: 1
+ */
+
+int print_string(va_list ap)
 {
-    unsigned int num = va_arg(my_args, unsigned int);
-    printf("%X", num);
+	char *str = va_arg(ap, char *);
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
 
-void printStringWithHex(va_list my_args)
+/**
+ * int_to_binary - turns unsined int to binary
+ * @c: unsigned int
+ *
+ * Return: 0 or 1
+ */
+
+int int_to_binary(int num)
 {
-    char *str = va_arg(my_args, char *);
-    while (*str)
-    {
-        if (*str < 32 || *str >= 127)
-        {
-            putchar('\\');
-            putchar('x');
-            printf("%02X", *str);
-        }
-        else
-        {
-            putchar(*str);
-        }
-        str++;
-    }
+	unsigned int count;
+
+	count = 0;
+
+	if (num / 2)
+		count += int_to_binary(num / 2);
+
+	count += _putchar((num % 2) + '0');
+
+	return (count);
 }
 
-void printChar(va_list my_args)
+/*int main()
 {
-    char c = (char)va_arg(my_args, int);
-    putchar(c);
-}
+	int num = 9;
+	decimalToBinary(num);
+	return 0;
+}*/
 
-void printString(va_list my_args)
-{
-    char *str = va_arg(my_args, char *);
-    while (*str)
-    {
-        putchar(*str);
-        str++;
-    }
-}
+/**
+ * int_to_bin - turns int to binary
+ * @ap: list of arguments taken
+ * Return: 1
+ */
 
-void printDecimal(va_list my_args)
+int int_to_bin(va_list ap)
 {
-    int num = va_arg(my_args, int);
-    printf("%d", num);
-}
+	int a = va_arg(ap, int);
 
-void printPointer(va_list my_args)
-{
-    void *ptr = va_arg(my_args, void *);
-    printf("%p", ptr);
-}
-
-void printLong(va_list my_args)
-{
-    long num = va_arg(my_args, long);
-    printf("%ld", num);
-}
-
-void printShort(va_list my_args)
-{
-    int num = va_arg(my_args, int);
-    printf("%hd", num);
-}
-
-void printReversedString(va_list my_args)
-{
-    char *str = va_arg(my_args, char *);
-    int len = 0;
-    int i;
-    while (str[len])
-        len++;
-    for (i = len - 1; i >= 0; i--)
-    {
-        putchar(str[i]);
-    }
-}
-
-void printRot13String(va_list my_args)
-{
-    char *str = va_arg(my_args, char *);
-    while (*str)
-    {
-        char c = *str;
-        if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-            c += 13;
-        else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-            c -= 13;
-        putchar(c);
-        str++;
-    }
+	return (int_to_binary(a));
 }
