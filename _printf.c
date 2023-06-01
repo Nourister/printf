39148824ea841a192086d5c4ea41eaf4efe01a08
nourister <nourusterjuma@gmail.com>
2023-05-31 23:24:14 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,21 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+
+/* Array of conversion specifiers and corresponding print functions */
+print_function_t print_functions[] = {
+    {"u", printUnsigned},
+    {"o", printOctal},
+    {"x", printHex},
+    {"X", printHexUpper},
+    {"S", printStringWithHex},
+    {"c", printChar},
+    {"s", printString},
+    {"d", printDecimal},
+    {"i", printDecimal},
+    {"p", printPointer},
+    {"l", printLong},
+    {"h", printShort},
+    {"r", printReversedString},
+    {"R", printRot13String},
+    {NULL, NULL}
+};
 
 /**
- * _printf - Custom printf function that supports conversion specifiers: c, s, %, d, i
+ * _printf - Custom printf function that supports different conversion specifiers
  * @format: The format string
  * @...: Variable number of arguments
  *
- * Return: The number of characters printed (excluding the null byte used to end output to strings)
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list my_args;
     int counter = 0;
-    int n;
-    char *str;
-    int num;
+    int n, l;
 
     va_start(my_args, format);
 
@@ -24,36 +44,28 @@ int _printf(const char *format, ...)
         if (format[n] == '%')
         {
             n++;
-            switch (format[n])
+            if (format[n] == '%')
             {
-                case 'c':
-                    putchar(va_arg(my_args, int));
+                putchar('%');
+		counter++;
+                continue;
+            }
+
+            for (l = 0; print_functions[l].specifier != NULL; l++)
+            {
+                if (format[n] == print_functions[l].specifier[0])
+                {
+                    print_functions[l].print_function(my_args);
                     counter++;
                     break;
-                case 's':
-                    str = va_arg(my_args, char *);
-                    while (*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        counter++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    counter++;
-                    break;
-                case 'd':
-                case 'i':
-                    num = va_arg(my_args, int);
-                    printf("%d", num);
-                    counter++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[n]);
-                    counter += 2;
-                    break;
+                }
+            }
+
+            if (print_functions[l].specifier == NULL)
+            {
+                putchar('%');
+                putchar(format[n]);
+                counter += 2;
             }
         }
         else
